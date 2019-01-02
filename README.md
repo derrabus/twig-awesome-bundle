fae2ee1736c3054bf9285e5bd3f3def6d1d404fc
Alexander M. Turek <me@derrabus.de>
2019-01-02 17:36:51 +0100

    Experimental support for FA 5.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://travis-ci.org/derrabus/twig-awesome-bundle.svg?branch=master)](https://travis-ci.org/derrabus/twig-awesome-bundle)
 
-This bundle integrates the icon font [Font Awesome 4](http://fontawesome.io/) into Twig. But instead of using CSS to
+This bundle integrates the icon font [Font Awesome 5](http://fontawesome.io/) into Twig. But instead of using CSS to
 render icons on the frontend, the icons are rendered as SVG images directly into the compiled Twig template.
 
 ## Example
@@ -11,5 +11,5 @@ This example renders a flag icon. Please note that you should wrap the icon in a
 adjust its size.
 
 ```html
-<div style="height: 10em;">{% fa flag %}</div>
+<div style="height: 10em;">{% fa regular flag %}</div>
 ```
--- src/IconLocator.php
+++ src/IconLocator.php
@@ -4,88 +4,35 @@ declare(strict_types=1);
 
 namespace Rabus\TwigAwesomeBundle;
 
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
-use Psr\Log\NullLogger;
 use Rabus\TwigAwesomeBundle\Exception\InvalidArgumentException;
-use Traversable;
-use XMLReader;
+use const DIRECTORY_SEPARATOR;
 
-final class IconLocator implements LoggerAwareInterface
+final class IconLocator
 {
-    use LoggerAwareTrait;
-
-    /**
-     * @var string
-     */
     private $fontAwesomePath;
-
-    /**
-     * @var array
-     */
-    private $iconCache;
 
     public function __construct(string $fontAwesomePath)
     {
         $this->fontAwesomePath = $fontAwesomePath;
-        $this->setLogger(new NullLogger());
     }
 
-    public function getSvg(string $id): string
+    public function getSvg(string $collection, string $id): string
     {
-        $this->compileCache();
+        $fileName = implode(DIRECTORY_SEPARATOR, [
+            $this->fontAwesomePath,
+             'svgs',
+            $collection,
+            $id.'.svg',
+        ]);
 
-        if (!isset($this->iconCache[$id])) {
-            throw new InvalidArgumentException('Could not find the requested glyph.');
+        if (!file_exists($fileName)) {
+            throw new InvalidArgumentException(sprintf(
+                'Could not find the requested glyph: %s of collection %s.',
+                $id,
+                $collection
+            ));
         }
 
-        return $this->iconCache[$id];
-    }
-
-    private function compileCache(): void
-    {
-        if (null !== $this->iconCache) {
-            return;
-        }
-
-        $svgByUnicode = iterator_to_array($this->parseWebfont());
-
-        $this->iconCache = [];
-        foreach ($this->parseLessVariables() as $id => $char) {
-            if (!isset($svgByUnicode[$char])) {
-                $this->logger->warning('Unmatched icon: '.$id);
-                continue;
-            }
-            $this->iconCache[$id] = $svgByUnicode[$char];
-        }
-    }
-
-    private function parseWebfont(): Traversable
-    {
-        $reader = new XMLReader();
-        $reader->open($this->fontAwesomePath.'/fonts/fontawesome-webfont.svg');
-
-        while ($reader->read()) {
-            if ('glyph' !== $reader->name || XMLReader::ELEMENT !== $reader->nodeType) {
-                continue;
-            }
-
-            yield $reader->getAttribute('unicode') => $reader->getAttribute('d');
-        }
-
-        $reader->close();
-    }
-
-    private function parseLessVariables(): Traversable
-    {
-        $file = fopen($this->fontAwesomePath.'/less/variables.less', 'r');
-
-        while ($line = fgets($file)) {
-            if (1 !== preg_match('/^@fa-var-([^:]+):\s+\"\\\\([^\"]+)\";$/', trim($line), $matches)) {
-                continue;
-            }
-
-            yield $matches[1] => mb_convert_encoding(hex2bin($matches[2]), 'UTF-8', 'UCS-2');
-        }
+        return file_get_contents($fileName);
     }
 }
