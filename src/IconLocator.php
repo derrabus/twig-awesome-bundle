b039a1acbfb897be0ba7ab20da8f31e1147d146d
Alexander M. Turek <me@derrabus.de>
2025-12-31 17:28:37 +0100

    Remove deprecated functionality (#27)

--- src/IconLocator.php
+++ src/IconLocator.php
@@ -12,11 +12,8 @@ final readonly class IconLocator
 {
     public function __construct(
         private string $fontAwesomePath,
-        private Filesystem $filesystem = new Filesystem(),
+        private Filesystem $filesystem,
     ) {
-        if (\func_num_args() < 2) {
-            trigger_deprecation('derrabus/twig-awesome-bundle', '4.5', 'Not passing an instance of %s as second argument when constructing %s is deprecated.', Filesystem::class, self::class);
-        }
     }
 
     public function getSvg(string $collection, string $id): string
