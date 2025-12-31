d1aedde9b433add90817de10ce8126eaed453ee4
Alexander M. Turek <me@derrabus.de>
2025-12-31 17:25:09 +0100

    Remove deprecated functionality

--- tests/IconLocatorTest.php
+++ tests/IconLocatorTest.php
@@ -4,24 +4,16 @@ declare(strict_types=1);
 
 namespace Rabus\TwigAwesomeBundle;
 
-use PHPUnit\Framework\Attributes\IgnoreDeprecations;
+use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
 
 final class IconLocatorTest extends TestCase
 {
-    #[IgnoreDeprecations]
-    public function testDeprecatedConstructor(): void
-    {
-        $this->expectUserDeprecationMessage('Since derrabus/twig-awesome-bundle 4.5: Not passing an instance of Symfony\Component\Filesystem\Filesystem as second argument when constructing Rabus\TwigAwesomeBundle\IconLocator is deprecated.');
-
-        new IconLocator('/tmp/foo');
-    }
-
+    #[DoesNotPerformAssertions]
     public function testValidConstructorCall(): void
     {
-        $this->expectNotToPerformAssertions();
-
+        // @phpstan-ignore new.resultUnused
         new IconLocator('/tmp/foo', self::createStub(Filesystem::class));
     }
 }
