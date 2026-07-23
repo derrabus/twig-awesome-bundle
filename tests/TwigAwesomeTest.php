bf62da6bd6f022aafb944f8cd986bcaf166e16b9
Alexander M. Turek <me@derrabus.de>
2026-07-23 16:00:50 +0200

    Fix CS

--- tests/TwigAwesomeTest.php
+++ tests/TwigAwesomeTest.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Kernel\BundleInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Kernel;
 use Twig\Environment;
